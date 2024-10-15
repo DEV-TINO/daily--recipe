4ec115e9f33e3c215d0489ea3d170ffef1d338ed
박태수 <tesupark00@gmail.com>
2024-10-15 23:54:56 +0900

    feat : get props data

--- src/components/RecipeListItem.vue
+++ src/components/RecipeListItem.vue
@@ -1,44 +1,60 @@
 <template>
-  <div class="list-item-contents">
-    <div class="card-thumbnail"></div>
-    <div class="list-item-info-container">
-      <div class="date-name">
-        <div class="date">yyyy-mm-dd</div>
-        <div class="list-item-user-name">username</div>
+  <div class="list-item">
+    <div class="thumbnail-container">
+      <img :src="previewData.thumbnail" class="thumbnail">
+    </div>
+    <div class="info-container">
+      <div class="metadata">
+        <div class="date">{{ previewData.created_at }}</div>
+        <div class="name">{{ previewData.username }}</div>
       </div>
-      <div class="title">title</div>
-      <div class="discription">subtitle</div>
+      <div class="title">{{ previewData.title }}</div>
+      <div class="discription">{{ previewData.subtitle }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    previewData: Object,
+  },
 }
 </script>
 
-<style>
-.list-item-contents {
+<style lang="scss">
+.list-item {
   margin-left: 30px;
   margin-right: 30px;
   display: grid;
   grid-template-columns: 100px 1fr;
   margin-bottom: 20px;
+  .thumbnail-container {
+    border-radius: 10px;
+    background-color: #999;
+    width: 100px;
+    height: 100px;
+    .thumbnail {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+      border-radius: 10px;
+    }
+  }
 }
-.date-name {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  align-items: center;
-}
-.list-item-info-container {
+.info-container {
   margin-left: 10px;
-}
-.list-item-user-name {
-  text-align: right;
-  font-size: 14px;
-}
-.date {
-  font-size: 10px;
+  .metadata {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    .name {
+      text-align: right;
+      font-size: 14px;
+    }
+    .date {
+      font-size: 10px;
+    }
+  }
 }
 </style>
