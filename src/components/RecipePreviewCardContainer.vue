e9fca953a473b5b9b95b769ff05fd8fb7c9357e6
박태수 <tesupark00@gmail.com>
2024-10-17 17:53:42 +0900

    feat : get data from HomeView.vue

--- src/components/RecipePreviewCardContainer.vue
+++ src/components/RecipePreviewCardContainer.vue
@@ -1,37 +1,12 @@
 <template>
   <div class="card-container">
-    <RecipePreviewCardVue v-for="(value, index) in previewContent" :key="index" :previewContent="previewContent[value.index]" @click="handleClickGoToDetail(value.id)"/>
+    <RecipePreviewCardVue v-for="(value, index) in cardData" :key="index" :previewContent="value" @click="handleClickGoToDetail(value.id)"/>
   </div>
 </template>
 
 <script>
 import RecipePreviewCardVue from './RecipePreviewCard.vue'
 export default {
-  data(){
-    return {
-      previewContent: [
-        {
-          index: 0,
-          id: "eggsoysaucerice",
-          title: "간장계란밥",
-          discription: "누구나 만들 수 있는 간장 계란밥 레시피 대공개",
-          thumbnail: "/mockdata/eggsoysaucerice/image/step_05.png",
-        },
-        {
-          index: 1,
-          title: "title2",
-          discription: "subtitle",
-          thumbnail: "",
-        },
-        {
-          index: 2,
-          title: "title3",
-          discription: "subtitle",
-          thumbnail: "",
-        },
-      ],
-    }
-  },
   components: {
     RecipePreviewCardVue: RecipePreviewCardVue,
   },
@@ -40,6 +15,9 @@ export default {
       const requestUrl = '/detail/'+recipeId
       this.$router.push(requestUrl)
     },
+  },
+  props: {
+    cardData: Array,
   }
 }
 </script>
