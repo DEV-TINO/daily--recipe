8e473bbaea49f8d2e27cdbacb4502afe0d361994
박태수 <tesupark00@gmail.com>
2024-10-17 16:39:05 +0900

    feat : get data from swipeContentsContainer

--- src/components/SwipeContents.vue
+++ src/components/SwipeContents.vue
@@ -1,29 +1,39 @@
 <template>
   <div class="large-content-view">
-    <div class="large-thumbnail"></div>
+    <div class="large-thumbnail">
+      <img :src="contentData.thumbnail" class="thumbnail">
+    </div>
     <div class="contents">
-      <div class="title">title</div>
-      <div class="discription">discriptions</div>
+      <div class="title">{{ contentData.title }}</div>
+      <div class="discription">{{ contentData.discription }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    contentData: Object,
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
 .large-content-view {
   margin-top: 10px;
   display: grid;
-  grid-template-columns: 180px 0fr;
-}
-.large-thumbnail {
-  width: 180px;
-  height: 180px;
-  border-radius: 10px;
-  background-color: #999999;
+  grid-template-columns: 180px 1fr;
+  .large-thumbnail {
+    border-radius: 10px;
+    background-color: #999;
+    width: 180px;
+    height: 180px;
+    .thumbnail {
+      object-fit: cover;
+      height: 100%;
+      width: 100%;
+      border-radius: 10px;
+    }
+  }
 }
 </style>
